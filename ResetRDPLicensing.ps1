a302d7c67fd98a0514fe434af48224859454857a
proceph <bricevanaken@gmail.com>
2024-08-02 13:00:50 +0200

    V1-ENG

--- ResetRDPLicensing.ps1
+++ ResetRDPLicensing.ps1
@@ -1,6 +1,6 @@
-#Script pour reset la période d'essai du RDP, à utiliser en dev seulement
+#Script for resetting rdp licensing 120 days grace period, to use only in dev env, not in prod env
 
-#Attribution des droits et suppression de la clé: 
+#Granting access and deleting registry grace period key
 $key = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey("SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod", [Microsoft.Win32.RegistryKeyPermissionCheck]::ReadWriteSubTree,[System.Security.AccessControl.RegistryRights]::takeownership)
 $acl = $key.GetAccessControl()
 $user = whoami
@@ -10,7 +10,7 @@ $acl.SetAccessRule($rule)
 $key.SetAccessControl($acl)
 Remove-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod'
 
-# puis relancer les deux services
+# Restarting rdp services
 net stop TermService /y
 net start TermService /y
 
